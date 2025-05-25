5a4a19f803e8fff18e22270d1377c40f69b5b1b7
pz <2912150017@qq.com>
2025-05-25 22:26:15 +0800

    up

---
+++ htmlcomponent/component_test.go
@@ -0,0 +1,33 @@
+package htmlcomponent_test
+
+import (
+	"testing"
+
+	"github.com/suifengpiao14/htmltemplate/htmlcomponent"
+)
+
+func TestComponent(t *testing.T) {
+	component := htmlcomponent.Component{
+		ComponentName: "xyxzapi/index",
+		Template: `
+		<div class="flex flex-col w-full ">
+		背景色-黄色
+		<suifengpiao14:tab config="orderTypeTab" />
+		</div>
+		`,
+		DataTpl: `
+	{
+		"orderTypeTab":{
+				"namespace":"orderTypeTab",
+				"eventName":"",
+				"activeTabId":"arrive_shop",
+				"items":[
+					{"tabId":"arrive_shop","tabTitle":"到店订单","tabContent":"{{{arriveShopContentOutput}}}"},
+					{"tabId":"arrive_home","tabTitle":"上门订单","tabContent":"{{{arriveHomeContentOutput}}}"}
+				]
+	}
+	}
+		`,
+	}
+	_ = component
+}
