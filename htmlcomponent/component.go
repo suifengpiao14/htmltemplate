d5c816818698a245ebd5ad8af833f06dd9e814e9
zheng peng <pengzheng@huishoubao.com.cn>
2025-05-28 09:28:49 +0800

    up

--- htmlcomponent/component.go
+++ htmlcomponent/component.go
@@ -11,10 +11,10 @@ type Component struct {
 	Attributes Attributes
 }
 
-func NewComponent(name string, slotNames Slots, components Templates, attributes Attributes) *Component {
+func NewComponent(name string, slots Slots, components Templates, attributes Attributes) *Component {
 	return &Component{
 		Name:       name,
-		Slots:      slotNames,
+		Slots:      slots,
 		Templates:  components,
 		Attributes: attributes,
 	}
@@ -22,14 +22,14 @@ func NewComponent(name string, slotNames Slots, components Templates, attributes
 
 func (p Component) Render(data map[string]any) (rootComponentHtml string, err error) {
 	componentName := p.Name
-	nodes := p.Slots
+	slots := p.Slots
 	templates := p.Templates
-	variables, err := nodes.RenderTemplate(templates, p.Attributes, data)
+	variables, err := slots.RenderTemplate(templates, p.Attributes, data)
 	if err != nil {
 		return "", err
 	}
 
-	rootNode, err := nodes.GetRootNode(componentName)
+	rootNode, err := slots.GetRootNode(componentName)
 	if err != nil {
 
 		return "", err
